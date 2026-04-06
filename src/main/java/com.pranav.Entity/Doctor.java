c8c3ff8ec83132a5f94636f07c8f0aec9564f5c7
Pranav Gaira <148996644+pranav045@users.noreply.github.com>
2026-04-06 18:08:31 +0530

    Add Doctor entity with fields and methods

--- src/main/java/com.pranav.Entity/Doctor.java
+++ src/main/java/com.pranav.Entity/Doctor.java
@@ -1,1 +1,38 @@
+package com.pranav.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Doctor {
+	@Id
+	private int id;
+	private String name;
+	private String specification;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSpecification() {
+		return specification;
+	}
+
+	public void setSpecification(String specification) {
+		this.specification = specification;
+	}
+
+}
 
