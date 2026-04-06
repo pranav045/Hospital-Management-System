0641333f11d83139d8164f2b765271bdf296d206
Pranav Gaira <148996644+pranav045@users.noreply.github.com>
2026-04-06 18:15:20 +0530

    Add Appointment entity with relationships to Doctor and Patient

--- src/main/java/com.pranav.Entity/Appointment.java
+++ src/main/java/com.pranav.Entity/Appointment.java
@@ -1,1 +1,53 @@
+package com.pranav.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Appointment {
+	@Id
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Doctor getDoctor() {
+		return doctor;
+	}
+
+	public void setDoctor(Doctor doctor) {
+		this.doctor = doctor;
+	}
+
+	public Patient getPatient() {
+		return patient;
+	}
+
+	public void setPatient(Patient patient) {
+		this.patient = patient;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	@ManyToOne
+	@JoinColumn(name = "doctor_id")
+	private Doctor doctor;
+	@ManyToOne
+	@JoinColumn(name = "patient_id")
+	private Patient patient;
+	private String date;
+}
 
