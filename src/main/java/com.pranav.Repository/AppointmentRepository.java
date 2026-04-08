c64d48b174393d2119abd40dc730f9be31398f6e
Pranav Gaira <148996644+pranav045@users.noreply.github.com>
2026-04-08 09:17:16 +0530

    Add @Repository annotation to AppointmentRepository

--- src/main/java/com.pranav.Repository/AppointmentRepository.java
+++ src/main/java/com.pranav.Repository/AppointmentRepository.java
@@ -3,7 +3,7 @@ package com.pranav.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.pranav.Entity.Appointment;
-
+@Repository
 public interface AppointmentRepository extends JpaRepository<Appointment, Integer> {
 
 }
