341271944940e870ae2847120aee7bfd377da672
Pranav Gaira <148996644+pranav045@users.noreply.github.com>
2026-04-08 09:18:37 +0530

    Add @Repository annotation to PatientRepository

--- src/main/java/com.pranav.Repository/PatientRepository.java
+++ src/main/java/com.pranav.Repository/PatientRepository.java
@@ -3,6 +3,7 @@ package com.pranav.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.pranav.Entity.Patient;
+@Repository
 public interface PatientRepository extends JpaRepository<Patient,Integer>{
   
 }
