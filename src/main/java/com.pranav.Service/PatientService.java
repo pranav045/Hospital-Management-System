5bca4675f0e45cad6ebf1e97a59a918e557d2f83
Pranav Gaira <148996644+pranav045@users.noreply.github.com>
2026-04-11 13:18:35 +0530

    Implement PatientService with add and get methods

--- src/main/java/com.pranav.Service/PatientService.java
+++ src/main/java/com.pranav.Service/PatientService.java
@@ -1,1 +1,35 @@
+package com.pranav.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.pranav.DAO.PatientDAO;
+import com.pranav.DTO.ResponseStructure;
+import com.pranav.Entity.Patient;
+
+@Service
+public class PatientService {
+	@Autowired
+	private PatientDAO patientDAO;
+
+	public ResponseEntity<ResponseStructure<Patient>> addPatient(Patient patient) {
+		Patient receivedPatient = patientDAO.savePatient(patient);
+		ResponseStructure<Patient> rs = new ResponseStructure<Patient>();
+		rs.setStatusCode(HttpStatus.CREATED.value());
+		rs.setData(receivedPatient);
+		rs.setMessage("Patient created Successfully");
+		return new ResponseEntity<ResponseStructure<Patient>>(rs, HttpStatus.CREATED);
+	}
+
+	public ResponseEntity<ResponseStructure<Patient>> getAppointment(int id) {
+		Patient data = patientDAO.getPatient(id);
+		ResponseStructure<Patient> rs = new ResponseStructure<Patient>();
+		rs.setStatusCode(HttpStatus.FOUND.value());
+		rs.setData(data);
+		rs.setMessage("Patient found successfullly");
+		return new ResponseEntity<ResponseStructure<Patient>>(rs, HttpStatus.FOUND);
+	}
+}
 
