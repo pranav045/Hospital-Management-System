819dfb865620b2e5b35d5dad1de666c0edf7c32a
Pranav Gaira <148996644+pranav045@users.noreply.github.com>
2026-05-16 10:00:10 +0530

    Add exception handler for EmptyException

--- src/main/java/com.pranav.Exception/AppointmentExceptionHandler.java
+++ src/main/java/com.pranav.Exception/AppointmentExceptionHandler.java
@@ -17,4 +17,13 @@ public class AppointmentExceptionHandler {
 		rs.setStatusCode(HttpStatus.NOT_FOUND.value());
 		return new ResponseEntity<ResponseStructure<String>>(rs, HttpStatus.NOT_FOUND);
 	}
+
+	@ExceptionHandler(EmptyException.class)
+	public ResponseEntity<ResponseStructure<String>> catchEmptyException(EmptyException exception) {
+		ResponseStructure<String> rs = new ResponseStructure<String>();
+		rs.setData(exception.getMessage());
+		rs.setMessage("No Appointments found");
+		rs.setStatusCode(HttpStatus.NOT_FOUND.value());
+		return new ResponseEntity<ResponseStructure<String>>(rs, HttpStatus.NOT_FOUND);
+	}
 }
