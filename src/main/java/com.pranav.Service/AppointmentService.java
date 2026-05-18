f5100117dcb4684464515f9bcb5530372edead96
Pranav Gaira <148996644+pranav045@users.noreply.github.com>
2026-05-18 18:41:37 +0530

    Refactor AppointmentService for improved response handling

--- src/main/java/com.pranav.Service/AppointmentService.java
+++ src/main/java/com.pranav.Service/AppointmentService.java
@@ -1,5 +1,7 @@
 package com.pranav.Service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Service;
 import com.pranav.DAO.AppointmentDAO;
 import com.pranav.DTO.ResponseStructure;
 import com.pranav.Entity.Appointment;
+import com.pranav.Exception.EmptyException;
 import com.pranav.Exception.IdDoesNotPresentException;
 
 @Service
@@ -17,23 +20,36 @@ public class AppointmentService {
 
 	public ResponseEntity<ResponseStructure<Appointment>> saveAppointment(Appointment appointment) {
 		Appointment data = appointmentDAO.saveAppointment(appointment);
-		ResponseStructure<Appointment> rs = new ResponseStructure<Appointment>();
+		ResponseStructure<Appointment> rs = new ResponseStructure<>();
 		rs.setStatusCode(HttpStatus.CREATED.value());
 		rs.setData(data);
 		rs.setMessage("Created Successfully");
-		return new ResponseEntity<ResponseStructure<Appointment>>(rs, HttpStatus.CREATED);
+		return new ResponseEntity<>(rs, HttpStatus.CREATED);
 	}
 
 	public ResponseEntity<ResponseStructure<Appointment>> getAppointment(int id) {
 		Appointment data = appointmentDAO.getAppointment(id);
 		if (data != null) {
-			ResponseStructure<Appointment> rs = new ResponseStructure<Appointment>();
-			rs.setStatusCode(HttpStatus.FOUND.value());
+			ResponseStructure<Appointment> rs = new ResponseStructure<>();
+			rs.setStatusCode(HttpStatus.OK.value());
 			rs.setData(data);
-			rs.setMessage("Appointment found successfullly");
-			return new ResponseEntity<ResponseStructure<Appointment>>(rs, HttpStatus.FOUND);
+			rs.setMessage("Appointment found successfully");
+			return new ResponseEntity<>(rs, HttpStatus.OK);
 		} else {
 			throw new IdDoesNotPresentException("Id " + id + " not found");
+		}
+	}
+
+	public ResponseEntity<ResponseStructure<List<Appointment>>> getAllAppointments() {
+		List<Appointment> data = appointmentDAO.getAllAppointments();
+		if (data != null) {
+			ResponseStructure<List<Appointment>> rs = new ResponseStructure<List<Appointment>>();
+			rs.setData(data);
+			rs.setMessage("All appointments found successfully");
+			rs.setStatusCode(HttpStatus.FOUND.value());
+			return new ResponseEntity<ResponseStructure<List<Appointment>>>(rs, HttpStatus.FOUND);
+		} else {
+			throw new EmptyException("No appointments found");
 		}
 	}
 }
