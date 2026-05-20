6b9350b80e5feb5faf20e16bbbc0844e6a899ccd
Pranav Gaira <148996644+pranav045@users.noreply.github.com>
2026-05-20 08:36:55 +0530

    Refactor AppointmentController endpoints and add delete method

--- src/main/java/com.pranav.Controller/AppointmentController.java
+++ src/main/java/com.pranav.Controller/AppointmentController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,7 +25,7 @@ public class AppointmentController {
 		return appointmentService.saveAppointment(appointment);
 	}
 
-	@GetMapping("/get/{id}")
+	@GetMapping("/getAppointment/{id}")
 	public ResponseEntity<ResponseStructure<Appointment>> getAppointment(@PathVariable int id) {
 		return appointmentService.getAppointment(id);
 	}
@@ -33,4 +34,10 @@ public class AppointmentController {
 	public ResponseEntity<ResponseStructure<List<Appointment>>> getAllAppointments() {
 		return appointmentService.getAllAppointments();
 	}
+
+	@DeleteMapping("/deleteAppointment/{id}")
+	public ResponseEntity<ResponseStructure<Appointment>> deleteAppointment(@PathVariable int id) {
+		return appointmentService.deleteAppointment(id);
+	}
+
 }
