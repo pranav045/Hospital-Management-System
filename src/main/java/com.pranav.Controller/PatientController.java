e237dd1b564e444c553328d050f1dc42d7b17dd0
Pranav Gaira <148996644+pranav045@users.noreply.github.com>
2026-05-21 11:07:12 +0530

    Add deletePatient endpoint to PatientController

--- src/main/java/com.pranav.Controller/PatientController.java
+++ src/main/java/com.pranav.Controller/PatientController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,5 +33,10 @@ public class PatientController {
 	@GetMapping("/getAllPatients")
 	public ResponseEntity<ResponseStructure<List<Patient>>> getAllPatients() {
 		return patientService.getAllPatients();
+	}
+
+	@DeleteMapping("/deletePatient/{id}")
+	public ResponseEntity<ResponseStructure<Patient>> deletePatient(@PathVariable int id) {
+		return patientService.deletePatient(id);
 	}
 }
