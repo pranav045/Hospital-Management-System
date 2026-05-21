ac38f4b948a895769a2760c8aa813a851acc2ab9
Pranav Gaira <148996644+pranav045@users.noreply.github.com>
2026-05-21 11:02:28 +0530

    Add deleteDoctor endpoint to DoctorController

--- src/main/java/com.pranav.Controller/DoctorController.java
+++ src/main/java/com.pranav.Controller/DoctorController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,5 +33,10 @@ public class DoctorController {
 	@GetMapping("/getAllDoctors")
 	public ResponseEntity<ResponseStructure<List<Doctor>>> getAllDoctors() {
 		return doctorService.getAllDoctors();
+	}
+
+	@DeleteMapping("/deletePatient/{id}")
+	public ResponseEntity<ResponseStructure<Doctor>> deletePatient(@PathVariable int id) {
+		return doctorService.deleteDoctor(id);
 	}
 }
